9609ce09595593300d685872d1ce1a8fdf7859e1
rach-syahputra <rach.syahputra@gmail.com>
2025-03-28 12:15:23 +0800

    feat(api): update convertDateToUTC7 util function parameter

--- apps/api/src/helpers/utils.ts
+++ apps/api/src/helpers/utils.ts
@@ -14,6 +14,5 @@ export const generateHashedPassword = async (password: string) => {
 
 export const convertDateToUTC7 = (date: Date): string => {
   const utc7Date = new Date(date.getTime() + 7 * 60 * 60 * 1000);
-
   return utc7Date.toISOString();
 };
