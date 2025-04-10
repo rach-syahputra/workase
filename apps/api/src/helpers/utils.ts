bdf3bf4dc2a9b21c536b62696ab247c77adefead
connect-spesifickly <mozar.sosial@gmail.com>
2025-04-10 08:49:31 +0700

    fix(app): resolve conflict

--- apps/api/src/helpers/utils.ts
+++ apps/api/src/helpers/utils.ts
@@ -1,8 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 
-import { CompanyReviewRating } from '@/interfaces/company-review.interface';
-
 export const generateUUID = () => {
   return uuidv4();
 };
@@ -17,16 +15,4 @@ export const generateHashedPassword = async (password: string) => {
 export const convertDateToUTC7 = (date: Date): string => {
   const utc7Date = new Date(date.getTime() + 7 * 60 * 60 * 1000);
   return utc7Date.toISOString();
-};
-
-export const calculateOverallRating = ({
-  workCulture,
-  workLifeBalance,
-  facilities,
-  careerGrowth,
-}: CompanyReviewRating) => {
-  const overallRating =
-    (workCulture + workLifeBalance + facilities + careerGrowth) / 4;
-
-  return overallRating > 5 ? 5 : overallRating;
 };
--- apps/api/src/services/company-review.service.ts
+++ apps/api/src/services/company-review.service.ts
@@ -2,6 +2,7 @@ import {
   AddCompanyReviewRequest,
   GetCompaniesReviewsRequest,
 <<<<<<< HEAD
+<<<<<<< HEAD
   GetCompanyHeaderRequest,
   GetCompanyRatingRequest,
 =======
@@ -10,18 +11,38 @@ import {
   VerifyUserEmploymentRequest,
 } from '@/interfaces/company-review.interface';
 import CompanyReviewRepository from '@/repositories/company-review.repository';
+=======
+  GetCompanyRatingRequest,
+  GetCompanyReviewsRequest,
+  VerifyUserEmploymentRequest,
+} from '@/interfaces/company-review.interface';
+import AddCompanyReviewRepository from '@/repositories/company-reviews/add-company-review.repository';
+import GetCompanyReviewRepository from '@/repositories/company-reviews/get-company-review.repository';
+>>>>>>> 340f8de (feat(api): add Get Company Rating API)
 import { addCompanyReviewSchema } from '@/validations/company-review.validation';
 import GetCompanyRatingRepository from '@/repositories/company-reviews/get-company-rating.repository';
 import GetCompanyHeaderRepository from '@/repositories/company-reviews/get-company-header.repository';
 
 import { ResponseError } from '@/helpers/error';
 import { validate } from '@/helpers/validation';
+import GetCompanyRatingRepository from '@/repositories/company-reviews/get-company-rating.repository';
 
 class CompanyReviewService {
+<<<<<<< HEAD
   private companyReviewRepository: CompanyReviewRepository;
 
   constructor() {
     this.companyReviewRepository = new CompanyReviewRepository();
+=======
+  private addCompanyReviewRepository: AddCompanyReviewRepository;
+  private getCompanyReviewRepository: GetCompanyReviewRepository;
+  private getCompanyRatingRepository: GetCompanyRatingRepository;
+
+  constructor() {
+    this.addCompanyReviewRepository = new AddCompanyReviewRepository();
+    this.getCompanyReviewRepository = new GetCompanyReviewRepository();
+    this.getCompanyRatingRepository = new GetCompanyRatingRepository();
+>>>>>>> 340f8de (feat(api): add Get Company Rating API)
   }
 
   verifyUserEmployment = async (req: VerifyUserEmploymentRequest) => {
@@ -59,12 +80,15 @@ class CompanyReviewService {
     });
   };
 
+<<<<<<< HEAD
   getCompanyHeader = async (req: GetCompanyHeaderRequest) => {
     return this.getCompanyHeaderRepository.getCompanyHeader({
       companyId: req.companyId,
     });
   };
 
+=======
+>>>>>>> 340f8de (feat(api): add Get Company Rating API)
   getCompanyRating = async (req: GetCompanyRatingRequest) => {
     return this.getCompanyRatingRepository.getCompanyRating({
       companyId: req.companyId,
