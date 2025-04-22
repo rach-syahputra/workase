f82ab67353c5e47c58ecbbcec47e29ca0976e5ae
connect-spesifickly <mozar.sosial@gmail.com>
2025-04-22 20:26:50 +0700

    refactor(auth): simplify logic, remove duplication and unused functions in auth.ts

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,9 +1,17 @@
 import NextAuth, { Account, Profile, User } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import Google from 'next-auth/providers/google';
-import { axiosPublic } from './lib/axios';
 import { jwtDecode } from 'jwt-decode';
 import GoogleProvider from 'next-auth/providers/google';
+import {
+  loginCompanyWithEmail,
+  loginCompanyWithGoogle,
+  loginUserWithEmail,
+  loginUserWithGoogle,
+  refreshCompanyToken,
+  refreshUserToken,
+  registerUserWithGoogle,
+} from './lib/apis/authentication';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: 'jwt',
@@ -44,107 +52,27 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       id: 'user-login',
       name: 'User Credentials',
       async authorize(credentials) {
-        try {
-          const response = await axiosPublic.post('/auth/login/user', {
-            email: credentials.email,
-            password: credentials.password,
-            authProvider: 'EMAIL',
-          });
-          const user = response.data.data;
-          user.type = 'user';
-          return user;
-        } catch (error) {
-          return null;
-        }
+        const user = await loginUserWithEmail(
+          credentials.email as string,
+          credentials.password as string,
+        );
+        return user;
       },
     }),
     Credentials({
       id: 'company-login',
       name: 'Company Credentials',
       async authorize(credentials) {
-        try {
-          const response = await axiosPublic.post('/auth/login/company', {
-            email: credentials.email,
-            password: credentials.password,
-            authProvider: 'EMAIL',
-          });
-          const user = response.data.data;
-          user.type = 'company';
-          return user;
-        } catch (error) {
-          return null;
-        }
+        const user = await loginCompanyWithEmail(
+          credentials.email as string,
+          credentials.password as string,
+        );
+        return user;
       },
     }),
   ],
   secret: process.env.AUTH_SECRET,
   callbacks: {
-    async jwt({ token, user, trigger }) {
-      if (user) {
-        token.accessToken = user.accessToken;
-        token.refreshToken = user.refreshToken;
-        token.type = user.type;
-      } else if (token.accessToken || trigger === 'update') {
-        const { type } = token as { type: string };
-        if (type == 'user') {
-          const newToken = await axiosPublic.post('/users/token', undefined, {
-            headers: {
-              Authorization: `Bearer ${token.refreshToken}`,
-            },
-          });
-          token.accessToken = newToken.data.data.accessToken;
-          token.refreshToken = newToken.data.data.refreshToken;
-          token.type = token.type;
-        } else {
-          const newToken = await axiosPublic.post(
-            '/companies/token',
-            undefined,
-            {
-              headers: {
-                Authorization: `Bearer ${token.refreshToken}`,
-              },
-            },
-          );
-          token.accessToken = newToken.data.data.accessToken;
-          token.refreshToken = newToken.data.data.refreshToken;
-          token.type = token.type;
-        }
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token.accessToken) {
-        const user = jwtDecode(token.accessToken as string);
-        if (user.role === 'USER') {
-          session.user.role = user.role;
-          session.user.id = user.id as string;
-          session.user.email = user.email as string;
-          session.user.authProvider = user.authProvider;
-          session.user.isVerified = user.isVerified as boolean;
-          session.user.location = user.location ?? '';
-          session.user.profilePhoto = user.profilePhoto as string;
-          session.user.placeOfBirth = user.placeOfBirth;
-          session.user.gender = user.gender;
-          session.user.lastEducation = user.lastEducation;
-          session.user.address = user.address;
-          session.user.jobId = user.jobId;
-        } else if (user.role === 'ADMIN') {
-          session.user.role = user.role;
-          session.user.id = user.id as string;
-          session.user.email = user.email as string;
-          session.user.authProvider = user.authProvider;
-          session.user.isVerified = user.isVerified;
-          session.user.location = user.location;
-          session.user.logoUrl = user.logoUrl;
-          session.user.descipton = user.descipton;
-          session.user.category = user.category;
-          session.user.name = user.name;
-          session.user.phoneNumber = user.phoneNumber;
-          session.user.slug = user.slug;
-        }
-      }
-      return session;
-    },
     async signIn({
       user,
       account,
@@ -160,39 +88,76 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       ) {
         return true;
       }
+      const { email } = profile as Profile;
       if (user.type == 'user') {
         try {
-          const response = await axiosPublic.post('/auth/login/user', {
-            email: profile?.email,
-            authProvider: 'GOOGLE',
-          });
+          const response = await loginUserWithGoogle(email as string);
           user.accessToken = response.data.data.accessToken;
           user.refreshToken = response.data.data.refreshToken;
           user.type = 'user';
+          console.log('ini user', user);
         } catch (e) {
-          await axiosPublic.post('/auth/register/user', {
-            email: profile?.email,
-            authProvider: 'GOOGLE',
-          });
+          await registerUserWithGoogle(email as string);
         }
         return true;
       } else if (user.type == 'company') {
         try {
-          const response = await axiosPublic.post('/auth/login/company', {
-            email: profile?.email,
-            authProvider: 'GOOGLE',
-          });
+          const response = await loginCompanyWithGoogle(email as string);
           user.accessToken = response.data.data.accessToken;
           user.refreshToken = response.data.data.refreshToken;
           user.type = 'company';
         } catch (e) {
-          await axiosPublic.post('/auth/register/company', {
-            email: profile?.email,
-            authProvider: 'GOOGLE',
-          });
+          await registerUserWithGoogle(email as string);
         }
       }
       return true;
+    },
+    async jwt({ token, user, trigger }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.refreshToken = user.refreshToken;
+        token.type = user.type;
+      } else if (token.accessToken || trigger === 'update') {
+        const { type } = token as { type: string };
+        if (type == 'user') {
+          const newToken = await refreshUserToken(token.refreshToken as string);
+          token.accessToken = newToken.accessToken;
+          token.refreshToken = newToken.refreshToken;
+          token.type = token.type;
+        } else {
+          const newToken = await refreshCompanyToken(
+            token.refreshToken as string,
+          );
+          token.accessToken = newToken.accessToken;
+          token.refreshToken = newToken.refreshToken;
+          token.type = token.type;
+        }
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.accessToken) {
+        const user = jwtDecode(token.accessToken as string);
+        session.user.role = user.role;
+        session.user.isVerified = user.isVerified as boolean;
+        session.user.location = user.location ?? '';
+        session.user.profilePhoto = user.profilePhoto as string;
+        session.user.placeOfBirth = user.placeOfBirth;
+        session.user.gender = user.gender;
+        session.user.lastEducation = user.lastEducation;
+        session.user.address = user.address;
+        session.user.jobId = user.jobId;
+        session.user.id = user.id as string;
+        session.user.email = user.email as string;
+        session.user.authProvider = user.authProvider;
+        session.user.logoUrl = user.logoUrl;
+        session.user.descipton = user.descipton;
+        session.user.category = user.category;
+        session.user.name = user.name;
+        session.user.phoneNumber = user.phoneNumber;
+        session.user.slug = user.slug;
+      }
+      return session;
     },
   },
 });
