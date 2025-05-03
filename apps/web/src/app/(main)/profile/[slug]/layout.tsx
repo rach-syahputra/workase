1f810b9831782ef628110b4312d465e9b71f92fe
rach-syahputra <rach.syahputra@gmail.com>
2025-05-03 21:22:20 +0800

    feat(web): remove profile page

--- apps/web/src/app/(main)/profile/[slug]/layout.tsx
+++ apps/web/src/app/(main)/profile/[slug]/layout.tsx
@@ -1,27 +1,28 @@
-// import React from 'react';
+import React from 'react';
 
-// import { UserDetailProvider } from '@/context/user-detail-context';
-// import Container from '@/components/layout/container';
-// import ProfileMenu from './_components/profile-menu';
+import { UserDetailProvider } from '@/context/user-detail-context';
+import Container from '@/components/layout/container';
+import ProfileMenu from './_components/profile-menu';
 
-// interface ProfilePageProps {
-//   params: { slug: string };
-//   children: React.ReactNode;
-// }
+interface ProfilePageProps {
+  params: { slug: string };
+  children: React.ReactNode;
+}
 
-// const ProfileLayout = ({ params, children }: ProfilePageProps) => {
-//   const slug = params.slug;
+const ProfileLayout = ({ params, children }: ProfilePageProps) => {
+  const slug = params.slug;
 
-//   alert(`SLUG: ${slug}`);
+  window.alert(`SLUG: ${slug}`);
 
-//   return (
-//     <UserDetailProvider slug={slug}>
-//       <Container className="flex min-h-[calc(100svh-70px)] w-full flex-col items-start gap-6 p-0 lg:grid lg:grid-cols-7 lg:px-8">
-//         <ProfileMenu className="col-span-2 w-full max-lg:hidden" />
-//         <div className="w-full pb-8 lg:col-span-5 lg:mt-4">{children}</div>
-//       </Container>
-//     </UserDetailProvider>
-//   );
-// };
+  return (
+    // <UserDetailProvider slug={slug}>
+    //   <Container className="flex min-h-[calc(100svh-70px)] w-full flex-col items-start gap-6 p-0 lg:grid lg:grid-cols-7 lg:px-8">
+    //     <ProfileMenu className="col-span-2 w-full max-lg:hidden" />
+    //     <div className="w-full pb-8 lg:col-span-5 lg:mt-4">{children}</div>
+    //   </Container>
+    // </UserDetailProvider>
+    <div>Profile page</div>
+  );
+};
 
-// export default ProfileLayout;
+export default ProfileLayout;
--- apps/web/src/app/(main)/profile/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-// import PageContent from './_components/page-content';
-
-// const ProfilePage = async () => {
-//   return <PageContent />;
-// };
-
-// export default ProfilePage;
