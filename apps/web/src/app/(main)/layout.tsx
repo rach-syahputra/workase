51557868fd292449f035c585bc4eda7af284b31f
rach-syahputra <rach.syahputra@gmail.com>
2025-05-03 23:35:22 +0800

    feat: trigger redeploy

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -9,10 +9,11 @@ export default async function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <NavigationBar />
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
 }
+//
