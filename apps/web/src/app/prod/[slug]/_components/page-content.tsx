953b18cd49eca0d70b85b5b33a0b304c13eacbaa
rach-syahputra <rach.syahputra@gmail.com>
2025-05-04 11:00:06 +0800

    feat(web): update prod page

--- apps/web/src/app/prod/[slug]/_components/page-content.tsx
+++ apps/web/src/app/prod/[slug]/_components/page-content.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { NEXTAUTH_SECRET } from '@/lib/constants/constants';
+import { useEffect } from 'react';
 
 function PageContent() {
+  useEffect(() => {
+    console.log('env:', process.env.NEXTAUTH_SECRET);
+    console.log('constant:', NEXTAUTH_SECRET);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-4">
-      <span>{process.env.NEXTAUTH_SECRET}</span>
-      <span>{NEXTAUTH_SECRET}</span>
-    </div>
+    <div className="flex flex-col items-center justify-center gap-2 p-4"></div>
   );
 }
 
