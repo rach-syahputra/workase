e15b425b5dda1c8dc3275aec557f55f4bd8f6689
rach-syahputra <rach.syahputra@gmail.com>
2025-05-12 17:52:47 +0800

    fix: incorrect subscription started at on update payment

--- apps/web/src/context/user-stats-context/interface.ts
+++ apps/web/src/context/user-stats-context/interface.ts
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction } from 'react';
 import { IUserStats } from '@/lib/interfaces/user-stats';
 
 export interface IUserStatsContext {
+  update: boolean;
+  setUpdate: Dispatch<SetStateAction<boolean>>;
   userStats: IUserStats | undefined;
   setUserStats: Dispatch<SetStateAction<IUserStats | undefined>>;
   fetchGetUserStats: () => void;
