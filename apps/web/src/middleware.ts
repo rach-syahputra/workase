48f143fc6b57ddd64400b0702695214f6ed4b0bf
rach-syahputra <rach.syahputra@gmail.com>
2025-05-14 01:30:29 +0800

    feat(web): update middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,8 +4,6 @@ import { NextResponse } from 'next/server';
 export default middleware((req) => {
   if (!req.auth) {
     const homepageUrl = new URL('/', req.nextUrl.origin);
-    const callbackUrl = req.nextUrl.pathname + req.nextUrl.search;
-    homepageUrl.searchParams.set('redirect', callbackUrl);
 
     return NextResponse.redirect(homepageUrl);
   }
